75dd6de0ff11e47582238656abafeddf3fe29060
Quentin <quentin@affholder.fr>
2025-09-24 09:38:36 +0200

    add: paddle handling and bounce

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ let x = canvas.width/2;                      // position de la balle (axe des ab
 let y = canvas.height/2;     // centrage vertical dans le canvas
 let dx = 3;
 let dy = -3;
+let rx = canvas.width/2 -50;
+let ry = canvas.height - 30;
 let rafId;                       // identifiant de la boucle d'animation
 
 function drawBall() {
@@ -14,6 +16,11 @@ function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 15, 0, Math.PI * 2);
     ctx.fill();
+}
+
+function drawRectangle(){
+    ctx.fillStyle = "#0095DD";
+    ctx.fillRect(rx, ry, 100, 10);
 }
 
 function update() {
@@ -30,11 +37,28 @@ function update() {
         dy = -dy;
         dx += (Math.random()  - 0.5) * 2;
     }
+
+    if (y > canvas.height - 30 - 15 && x > rx && x < rx + 100) {
+        dy = -dy;
+        dx += (Math.random()  - 0.5) * 2;
+    }
+
     if (y > canvas.height - 15){
         gameOver();
     }
 
 }
+
+function moveRectangle(event) {
+    let direction = event.key;
+    if (direction === 'ArrowLeft' && rx > 0) {
+        rx -= 20;
+    }
+    else if (direction === 'ArrowRight' && rx < canvas.width - 100) {
+        rx += 20;
+    }
+}
+document.addEventListener('keydown', moveRectangle);
 
 function loop() {
     update();
@@ -42,12 +66,11 @@ function loop() {
     if  (rafId) {  // si l'animation est en cours
         rafId = requestAnimationFrame(loop);
     }
+    drawRectangle();
 }
 
 // démarrer l'animation
 loop();
-
-
 
 //             gestion des boutons
 
@@ -92,6 +115,7 @@ function gameOver() {
     alert("Game Over! La balle est tombée, votre score est de " + scoreElement.textContent.split(' ')[1]);
     scoreElement.textContent = 'Score: 0';
 }
+
 let scoreInterval;
 function compteurScore() {
     let score = 0;
